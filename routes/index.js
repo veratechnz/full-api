ec13b3460062f805bdf47226a3af2f534c83c2db
veratech <jared@veratech.co.nz>
2016-07-31 15:28:38 +1200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express It Mofo' });
+});
+
+/* GET home page. */
+router.get('/hi', function(req, res, next) {
+  res.render('hi', { title: 'Hello 31-07-2016' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+module.exports = router;
